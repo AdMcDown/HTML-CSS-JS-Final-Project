475c84a0698dda23c298331977a22683e319190d
Adam McNown <adammcnown@gmail.com>
2021-01-24 11:12:23 -0500

    making first year poject repo

---
+++ js/buy_now.js
@@ -0,0 +1,6 @@
+// When the user clicks on div, open the popup
+function myFunction() {
+var popup = document.getElementById("myPopup");
+popup.classList.toggle("show");
+}
+//Took Java script from https:www.w3schools.com/howto/howto_js_popup.asp and changed it to fit the page.
